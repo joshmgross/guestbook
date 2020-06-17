a7dc76e16fc0a89f98fcdc4f29b84716666b0a51
Josh Gross <joshmgross@github.com>
2020-06-17 02:57:25 +0000

    Milestone 1️⃣: List comments from an issue

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,77 @@
 import * as core from "@actions/core";
+import { context, getOctokit } from "@actions/github";
 
 import * as utils from "./utils";
+
+interface Comment {
+    id: number;
+    text: string;
+    user: string;
+    url: string;
+}
 
 async function run(): Promise<void> {
     try {
         // Inputs and validation
-        const myInput = core.getInput("my-input");
+        const token = core.getInput("token", { required: true });
+        const octokit = getOctokit(token);
 
-        utils.logInfo(`The specified input is ${myInput}`);
+        const issue = Number(core.getInput("issue", { required: true }));
+        if (isNaN(issue) || issue <= 0) {
+            core.setFailed("❌ Invalid input: issue must be a valid number.");
+            return;
+        }
+
+        utils.logInfo(`Retrieving issue commments from Issue #${issue}`);
+
+        const issueRequestData = {
+            // 🐫
+            // eslint-disable-next-line @typescript-eslint/camelcase
+            issue_number: issue,
+            owner: context.repo.owner,
+            repo: context.repo.repo
+        };
+
+        const issueComments: Comment[] = [];
+        for await (const issueCommentResponse of octokit.paginate.iterator(
+            octokit.issues.listComments,
+            issueRequestData
+        )) {
+            if (issueCommentResponse.status < 200 || issueCommentResponse.status > 299) {
+                core.error(
+                    `❌ Received error response when retrieving guestbook issue: ${
+                        issueCommentResponse.status
+                    } - ${JSON.stringify(issueCommentResponse.data)}.`
+                );
+                break;
+            }
+
+            issueComments.push(
+                ...issueCommentResponse.data.map(comment => {
+                    return {
+                        id: comment.id,
+                        text: comment.body,
+                        user: comment.user.login,
+                        url: comment.html_url
+                    } as Comment;
+                })
+            );
+        }
+
+        if (issueComments.length == 0) {
+            core.error("❌ No issues retrieved.");
+            return;
+        }
+
+        utils.logInfo(`Retrieved ${issueComments.length} issue comments.`);
+
+        for (const comment of issueComments) {
+            utils.logInfo(`@${comment.user} said "${comment.text}"`);
+        }
 
         utils.logInfo("🎉🎈🎊 Action complete 🎉🎈🎊");
     } catch (error) {
-        core.error(`❌ Action failed with error: ${error}`);
+        core.setFailed(`❌ Action failed with error: ${error}`);
     }
 }
 
